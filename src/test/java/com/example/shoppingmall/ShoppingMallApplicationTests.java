932f7a3338d0c8e85246c5713ff292f04ae78fe8
SudalKing <jmjang88@naver.com>
2023-10-29 13:38:44 +0900

    dev

--- src/test/java/com/example/shoppingmall/ShoppingMallApplicationTests.java
+++ src/test/java/com/example/shoppingmall/ShoppingMallApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.shoppingmall;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ShoppingMallApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
