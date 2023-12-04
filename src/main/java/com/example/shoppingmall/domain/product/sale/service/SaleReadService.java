dd5714623bf78b889f0dd480928399eac8ac27be
SudalKing <jmjang88@naver.com>
2023-12-04 15:43:51 +0900

    [BE]: refactoring

---
+++ src/main/java/com/example/shoppingmall/domain/product/sale/service/SaleReadService.java
@@ -0,0 +1,26 @@
+package com.example.shoppingmall.domain.product.sale.service;
+
+import com.example.shoppingmall.domain.product.product.entity.Product;
+import com.example.shoppingmall.domain.product.sale.entity.ProductSale;
+import com.example.shoppingmall.domain.product.sale.repository.ProductSaleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class SaleReadService {
+    private final ProductSaleRepository productSaleRepository;
+
+    public int getDiscountPrice(Product product) {
+        Optional<ProductSale> productSale = productSaleRepository.findByProductId(product.getId());
+
+        if (product.isSaled() && productSale.isPresent()) {
+            return (int) Math.round(product.getPrice() * productSale.get().getDiscountRate());
+        } else {
+            return 0;
+        }
+    }
+
+}
